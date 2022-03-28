37c3415104a69ad2f85b4666e88b046d1b03c96d
srikanthb524 <srikanthb@spyhre.com>
2022-03-28 12:52:29 +0530

    EOD 28-03-2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,81 @@
-import { Component } from '@angular/core';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit, ViewChild, ViewChildren } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'validations-ang';
-  model: any = {};
+
+  @ViewChild('contactForm')
+  contactForm!: NgForm;
+
+  countryList: country[] = [
+    new country('1', 'India'),
+    new country('2', 'USA'),
+    new country('3', 'UK'),
+  ];
+  contact!: contact;
+  ngOnInit(){
+    this.contact={
+      fname:"Srikanth",
+      lname:"B",
+      email:"sri524@gmail.com",
+      gender:"male",
+      isMarried:true,
+      country:"2",
+      address:{
+        city:"Rajahmundry",
+        street:"XYZ Street",
+        pincode:"533105"
+      }
+    }
+
+  }
+  onSubmit()
+  {
+console.log(this.contact);
+  }
+
+  patchValue()
+  {
+    let obj={
+      fname:"Krishna",
+      lname:'S',
+      email:"ks@gmail.com"
+    };
+    this.contactForm.control.patchValue(obj);
+    this.contactForm.controls["country"].setValue("1");
+  }
+  reset()
+  {
+   // this.contactForm.resetForm();
+     this.contactForm.reset();
+  }
+
+}
+export class country {
+  id: string = '';
+  name: string = '';
+  constructor(ID: string, name: string) {
+    this.id = ID;
+    this.name = name;
+  }
+}
+export class contact {
+  fname: string = '';
+  lname: string = '';
+  email: string = '';
+  gender: string = '';
+  isMarried: boolean = true;
+  country: string = '';
+  address!: {
+    city: string;
+    street: string;
+    pincode: string;
+  };
 }
